0dcd22223e10500038e7e957a8322bf93e40c7a2
Binod Tamang <bdtamang@braindigit.com>
2016-07-01 17:00:40 +0545

    added tryouts

---
+++ ZeroOne/ZeroOne/Other.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroOne
+{
+    public class Other
+    {
+    }
+    public class QWER<T>
+    {
+        protected T x;
+    }
+
+    public class MyQWER<T> : QWER<T>
+    {
+        private static void F()
+        {
+            MyQWER<T> dt = new MyQWER<T>();
+            MyQWER<int> x = new MyQWER<int>();
+            MyQWER<string> y = new MyQWER<string>();
+            dt.x = default(T);
+            x.x = 76;
+            y.x = "fun";
+        }
+    }
+
+
+    public class asdf
+    {
+        protected int x;
+
+        private static void funny(asdf a, asdf b)
+        {
+            a.x = 1;
+            b.x = 1;
+        }
+    }
+
+    public class childcl : asdf
+    {
+        private static void funny(asdf a, asdf b)
+        {
+            //a.x = 1;
+            //b.x = 1;
+        }
+    }
+
+    public interface IFun
+    {
+        void fun();
+        void fun(int x);
+        void fun(ref int x);
+        // int fun(int x);
+    }
+
+    public class test
+    {
+        private int x;
+        private static int y;
+
+        private void fun()
+        {
+            x = 1;
+            y = 2;
+            tt("1.0");
+        }
+
+        private static void funs()
+        {
+            y = 1;
+
+        }
+        private void tt(double x)
+        {
+        }
+        private void tt(object x)
+        {
+        }
+    }
+}
---
+++ ZeroOne/ZeroOne/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroOne
+{
+    class Program
+    {
+  
+        //public static string Convert(int n)
+        //{
+        //    return (n > 1 ? Convert(n / 2) : "") + n % 2;
+        //}
+        //public static List<string> Conversion2(int x)
+        //{
+        //    var bitConversion = new List<string>();
+        //    while (x >= 0)
+        //    {
+        //        if (x == 0)
+        //        {
+        //            bitConversion.Add("0");
+        //            break;
+        //        }
+        //        bitConversion.Add((x % 2).ToString(CultureInfo.InvariantCulture));
+        //        x /= 2;
+        //    }
+        //    bitConversion.Reverse();
+        //    return bitConversion;
+        //}
+    
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+
+                Console.Write("Enter your integer:");
+                int N = int.Parse(Console.ReadLine());
+
+                Int64 S = 0;
+                for (int i = 1; i < 100000; i++)
+                {
+                    S = Convert.ToInt64(Convert.ToString(i, 2));
+                    if (S%N == 0) break;
+                }
+
+                Console.WriteLine(S);
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.Write("Please entre integer only!");
+                }
+            }
+            ////Conversion2(4).ForEach(e => { Console.WriteLine(e); });
+            ////var ccc = string.Join("", Conversion2(999999));
+            ////ccc = ccc.TrimStart(new Char[] { '0' });
+            ////Console.WriteLine(ccc);
+            ////try
+            ////{
+            ////    Console.WriteLine(Solution(100));
+            ////}
+            ////catch (Exception)
+            ////{
+
+            ////    throw;
+            ////}
+            ////long a = 88888;
+
+            ////List<long> list = new List<long>();
+            ////list.Add(1);
+            ////bool flag = true;
+
+            ////while (flag)
+            ////{
+            ////    List<long> dummy = new List<long>();
+            ////    for (int i = 0; i < list.Count; i++)
+            ////    {
+            ////        var r = list[i] * 10;
+            ////        var r1 = r + 1;
+            ////        if (r % a == 0)
+            ////        {
+            ////            Console.WriteLine(r);
+            ////            flag = false;
+            ////            break;
+            ////        }
+            ////        dummy.Add(r);
+            ////        dummy.Add(r1);
+            ////    }
+            ////    list = dummy;
+            ////}
+
+
+
+        }
+    }
+}
